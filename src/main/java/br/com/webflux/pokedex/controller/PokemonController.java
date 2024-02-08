03c6a6c49817c722d4e6f26b52c150b055d16fee
Diogo Valente <diogovalente@hotmail.com.br>
2024-02-08 16:25:34 -0300

    Finalizando o CRUD de pokemon

---
+++ src/main/java/br/com/webflux/pokedex/controller/PokemonController.java
@@ -0,0 +1,61 @@
+package br.com.webflux.pokedex.controller;
+
+import br.com.webflux.pokedex.model.Pokemon;
+import br.com.webflux.pokedex.repository.PokemonRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class PokemonController {
+
+    private PokemonRepository pokemonRepository;
+
+    public PokemonController(PokemonRepository pokemonRepository) {
+        this.pokemonRepository = pokemonRepository;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Pokemon> savePokemon(@RequestBody Pokemon pokemon) {
+        return pokemonRepository.save(pokemon);
+    }
+
+    @GetMapping
+    public Flux<Pokemon> getAllPokemons() {
+        return pokemonRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<Pokemon>> getPokemonById(String id) {
+        return pokemonRepository.findById(id).
+                map(pokemon -> ResponseEntity.ok(pokemon)).defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public Mono<ResponseEntity<Pokemon>> updatePokemon(@PathVariable String id, @RequestBody Pokemon pokemon) {
+        return pokemonRepository.findById(id).flatMap(existingPokemon -> {
+            existingPokemon.setName(pokemon.getName());
+            existingPokemon.setHability(pokemon.getHability());
+            existingPokemon.setCategory(pokemon.getCategory());
+            existingPokemon.setWeight(pokemon.getWeight());
+            return pokemonRepository.save(existingPokemon);
+        })
+        .map(updatedPokemon -> ResponseEntity.ok(updatedPokemon))
+        .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<ResponseEntity<Void>> deletePokemon(@PathVariable String id) {
+        return pokemonRepository.findById(id)
+                .flatMap(existingPokemon -> pokemonRepository.delete(existingPokemon).then(Mono.just(ResponseEntity.ok().<Void>build())))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping
+    public Mono<Void> deleteAllPokemons() {
+        return pokemonRepository.deleteAll();
+    }
+}
